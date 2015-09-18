6e161bf43f047f4002b55c500bce0a4ebfb9eef5
Chris Culpepper <cculpepper1214@gmail.com>
2015-09-18 01:16:39 -0400

    Started a AX5043 header, added more to the.c

--- software/hardwareRev1Soft/ax5043.c
+++ software/hardwareRev1Soft/ax5043.c
@@ -68,3 +68,70 @@ CHUNK_HEAD_TXPWR 0xFD /* Allows the TX power to be changed on the fly. */
 
 
 
+PWRMODE_POWERDOWN 0x00 /* All circuits dead. Except the register. */ 
+PWRMODE_DEEPSLEEP 0x01 /* Now I really mean all circuits are dead. Data loss*/ 
+PWRMODE_STANDBY 0x05 /* Xtal OSC enabled*/ 
+PWRMODE_FIFOON 0x07  /* The FIFO and the crystal are enabled*/ 
+PWRMODE_SYNTHRX 0x08 /* The synth is running, in receive mode*/ 
+PWRMODE_FULLRX 0x09 /* The receiver is running*/ 
+PWRMODE_WORRX 0x0B /* Wake on radio mode*/ 
+PWRMODE_SYNTHTX 0x0C /* The synth is running, transmit mode*/ 
+PWRMODE_FULLTX 0x0D /* The transmitter is running. */ 
+
+int ax5043_full_tx(){
+	ax5043_sel();
+	 /* Need to put into full tx
+	  * Need to wait untill SVMODEM is high in the POWSTAT*/ 
+}
+
+int ax5043_autorange(){
+	/* This is needed if the frequency is changed more than 1MHz. 
+	 * Each frequency must be ranged, FREQA and B. 
+	 * Set power mode ta standby, enable TXCO
+	 * Wait until the osc is ready
+	 * Set RNGSTART of PLLRANGINGA/B
+	 * wait until RNGSTART = 0
+	 * If RNGERR == 1, error
+	 * esle set pwrmode to desired. */ 
+}
+int ax5043_send_data(char* tx_data, int len){
+	/* Set pwrmode to fulltx
+	 * enable tcxo
+	 * write pramble
+	 * write packet
+	 * wait for osc
+	 * commit fifo
+	 * wat for xmit to complete
+	 * set power mode*/ 
+}
+int ax5043_read_packet(char* rx_data, int max){
+	 /* Assumes a packet is ready and waiting
+	  * reads the header
+	  * reads the data until max len or data all read
+	  * empty the fifo if max is reached. */ 
+}
+AX_REG_REVISION 0x000
+AX_REG_PWRMODE 0x002
+AX_REG_POWSTAT 0x003
+AX_REG_POWSTICKYSTAT 0x004
+AX_REG_POWIRQMASK 0x005
+AX_REG_IRQMASK1 0x006
+AX_REG_IRQMASK0 0x007
+AX_REG_RADIOEVENTMASK1 0x008
+AX_REG_RADIOEVENTMASK0 0x009
+AX_REG_IRQINVERSION1 0x00a
+AX_REG_IRQINVERSION0 0x00b
+AX_REG_IRQREQUEST1 0x00c
+AX_REG_ 0x00d
+AX_REG_ 0x00e
+AX_REG_ 0x00f
+AX_REG_ 0x010
+AX_REG_ 0x011
+AX_REG_ 0x012
+AX_REG_ 0x013
+AX_REG_ 0x014
+AX_REG_ 0x015
+AX_REG_ 0x016
+AX_REG_ 0x017
+AX_REG_ 0x018
+AX_REG_ 0x019
